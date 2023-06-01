477400287580f9aa6bdbe01f97905d120872d1bc
Krish Sridar <krishkhavin@gmail.com>
2023-06-01 09:55:48 -0700

    implemented basic dashboard with navBar, FeaturedCard, SessionCards, and SessionsList

--- src/client/components/Dashboard/Dashboard.jsx
+++ src/client/components/Dashboard/Dashboard.jsx
@@ -1,126 +1,54 @@
 import * as React from 'react';
-import { styled, alpha } from '@mui/material/styles';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import InputBase from '@mui/material/InputBase';
-import MenuIcon from '@mui/icons-material/Menu';
-import SearchIcon from '@mui/icons-material/Search';
-
-import AccountCircle from '@mui/icons-material/AccountCircle';
-import Switch from '@mui/material/Switch';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import FormGroup from '@mui/material/FormGroup';
-import MenuItem from '@mui/material/MenuItem';
-import Menu from '@mui/material/Menu';
-
-const Search = styled('div')(({ theme }) => ({
-  position: 'relative',
-  borderRadius: theme.shape.borderRadius,
-  backgroundColor: alpha(theme.palette.common.white, 0.15),
-  '&:hover': {
-    backgroundColor: alpha(theme.palette.common.white, 0.25),
-  },
-  marginLeft: 0,
-  width: '100%',
-  [theme.breakpoints.up('sm')]: {
-    marginLeft: theme.spacing(1),
-    width: 'auto',
-  },
-}));
-
-const SearchIconWrapper = styled('div')(({ theme }) => ({
-  padding: theme.spacing(0, 2),
-  height: '100%',
-  position: 'absolute',
-  pointerEvents: 'none',
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
-}));
-
-const StyledInputBase = styled(InputBase)(({ theme }) => ({
-  color: 'inherit',
-  '& .MuiInputBase-input': {
-    padding: theme.spacing(1, 1, 1, 0),
-    // vertical padding + font size from searchIcon
-    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-    transition: theme.transitions.create('width'),
-    width: '100%',
-    [theme.breakpoints.up('sm')]: {
-      width: '12ch',
-      '&:focus': {
-        width: '20ch',
-      },
-    },
-  },
-}));
-
+import Navbar from '../Navbar/Navbar.jsx';
+import Featured from '../FeaturedCard/Featured.jsx';
+import SessionCard from '../SessionCard/SessionCard.jsx';
+import './Dashboard.css';
+import Divider from '@mui/material/Divider';
+import Fab from '@mui/material/Fab';
+import AddIcon from '@mui/icons-material/Add';
 
 
 export default function Dashboard() {
-    const [anchorEl, setAnchorEl] = React.useState(null);
-
-      const handleMenu = (event) => {
-        setAnchorEl(event.currentTarget);
-      };
-    
-      const handleClose = () => {
-        setAnchorEl(null);
-      };
-  return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar>
-          <Typography
-            variant="h6"
-            noWrap
-            component="div"
-            sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
-          >
-            dogFish
-          </Typography>
-          <Search>
-            <SearchIconWrapper>
-              <SearchIcon />
-            </SearchIconWrapper>
-            <StyledInputBase
-              placeholder="Search…"
-              inputProps={{ 'aria-label': 'search' }}
-            />
-          </Search>
-              <IconButton
-                size="large"
-                aria-label="account of current user"
-                aria-controls="menu-appbar"
-                aria-haspopup="true"
-                onClick={handleMenu}
-                color="inherit"
-              >
-                <AccountCircle />
-              </IconButton>
-              <Menu
-                id="menu-appbar"
-                anchorEl={anchorEl}
-                anchorOrigin={{
-                  vertical: 'top',
-                  horizontal: 'right',
-                }}
-                keepMounted
-                transformOrigin={{
-                  vertical: 'top',
-                  horizontal: 'right',
-                }}
-                open={Boolean(anchorEl)}
-                onClose={handleClose}
-              >
-                <MenuItem onClick={handleClose}>Profile</MenuItem>
-                <MenuItem onClick={handleClose}>My account</MenuItem>
-              </Menu>
-        </Toolbar>
-      </AppBar>
-    </Box>
-  );
+    return (   
+        <div className="container">
+            <Navbar />
+            <div className="featuredContainer">
+                <div className="featuredAndCreate">
+                    <h4>Featured</h4>
+                    <Fab variant="extended" color="primary" aria-label="create">
+                        <AddIcon />
+                        Create Session
+                    </Fab>
+                </div>
+                <Divider />
+                <Featured />
+            </div>
+            <div className="sessionsContainer">
+                <h4>Activities near you</h4>
+                <Divider />
+                <div className="sessionsList">
+                    <SessionCard />
+                    <SessionCard />
+                    <SessionCard />
+                    <SessionCard />
+                    <SessionCard />
+                    <SessionCard />
+                    <SessionCard />
+                </div>
+            </div>
+            <div className="sessionsContainer">
+                <h4>Soccer</h4>
+                <Divider />
+                <div className="sessionsList">
+                    <SessionCard />
+                    <SessionCard />
+                    <SessionCard />
+                    <SessionCard />
+                    <SessionCard />
+                    <SessionCard />
+                    <SessionCard />
+                </div>
+            </div>
+        </div>
+    )
 }
