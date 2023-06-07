ad830bfcacd2394caa5d978e4a54786bcf6612f2
Srujan <srujanr40@gmail.com>
2023-06-07 10:26:42 -0700

    better useState

--- src/client/components/CreateSession/CreateSessionTextFields.jsx
+++ src/client/components/CreateSession/CreateSessionTextFields.jsx
@@ -2,58 +2,62 @@ import { useState } from "react";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 import { Tooltip } from "@mui/material";
-import { useDispatch } from 'react-redux';
-import Button from '@mui/material/Button';
-import Stack from '@mui/material/Stack';
+import { useDispatch } from "react-redux";
+import Button from "@mui/material/Button";
+import Stack from "@mui/material/Stack";
 import { createNewSession } from "../../actions";
-import './CreateSession.css'
+import "./CreateSession.css";
 import React from "react";
 
 const equipmentInfo = (
   <React.Fragment>
     <div>Enter information about the equipment in the following format:</div>
-    <div style={{textAlign: "center"}}>"Quantity, Equipment Name"</div><br />
+    <div style={{ textAlign: "center" }}>"Quantity, Equipment Name"</div>
+    <br />
     <div>Example:</div>
     <div>1, Baseball Bat</div>
     <div>4, Tennis Ball</div>
     <div>2, Hockey Stick</div>
     <div>1, Volleyball Net</div>
   </React.Fragment>
-)
+);
 
 export default function CreateSessionTextFields() {
-  const [session_name, setName] = useState("");
-  const [session_sport, setSport] = useState("");
-  const [session_description, setDescription] = useState("");
-  const [session_city, setCity] = useState("");
-  const [session_location, setLocation] = useState("");
-  const [session_equipment_needed, setEquipmentNeeded] = useState("");
-  const [session_players_needed, setPlayersNeeded] = useState("");
-
-  
+  const [session, setSession] = useState({
+    session_name: "",
+    session_sport: "",
+    session_description: "",
+    session_city: "",
+    session_location: "",
+    session_equipment_needed: "",
+    session_players_needed: "",
+  });
 
   const dispatch = useDispatch();
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    const new_session = { session_name, 
-        session_sport, 
-        session_description, 
-        session_city,
-        session_location,
-        session_equipment_needed,
-        session_players_needed,
-    }
+    const new_session = {
+      session_name,
+      session_sport,
+      session_description,
+      session_city,
+      session_location,
+      session_equipment_needed,
+      session_players_needed,
+    };
 
-    dispatch(createNewSession(new_session)) //add function here that handles what happens with the data at submit
-    setName("");
-    setSport("");
-    setDescription("");
-    setCity("");
-    setLocation("");
-    setEquipmentNeeded("");
-    setPlayersNeeded("");
+    dispatch(createNewSession(new_session)); //add function here that handles what happens with the data at submit
+    setSession({
+      session_name: "",
+      session_sport: "",
+      session_description: "",
+      session_city: "",
+      session_location: "",
+      session_equipment_needed: "",
+      session_players_needed: "",
+    });
   };
 
   return (
@@ -66,10 +70,26 @@ export default function CreateSessionTextFields() {
         noValidate
         autoComplete="off"
       >
-        <Box className="text-fields" sx={{
-          display: "flex", 
-          flexDirection: { xs: "column", sm: "column", md: "column", lg: "row",}}}>
-          <Box sx={{alignItems: 'center', justifyContent: 'center', width: { sm: 450, md: 300, lg: 400 }, paddingRight: 2}}>
+        <Box
+          className="text-fields"
+          sx={{
+            display: "flex",
+            flexDirection: {
+              xs: "column",
+              sm: "column",
+              md: "column",
+              lg: "row",
+            },
+          }}
+        >
+          <Box
+            sx={{
+              alignItems: "center",
+              justifyContent: "center",
+              width: { sm: 450, md: 300, lg: 400 },
+              paddingRight: 2,
+            }}
+          >
             <TextField
               required
               fullWidth
@@ -77,7 +97,12 @@ export default function CreateSessionTextFields() {
               label="Session Name"
               margin="normal"
               value={session_name}
-              onChange={(e) => setName(e.target.value)}
+              onChange={(e) =>
+                setSession((prevState) => ({
+                  ...prevState,
+                  session_name: e,
+                }))
+              }
             />
             <br />
 
@@ -88,7 +113,12 @@ export default function CreateSessionTextFields() {
               label="Sport"
               margin="normal"
               value={session_sport}
-              onChange={(e) => setSport(e.target.value)}
+              onChange={(e) =>
+                setSession((prevState) => ({
+                  ...prevState,
+                  session_sport: e,
+                }))
+              }
             />
             <br />
 
@@ -100,7 +130,12 @@ export default function CreateSessionTextFields() {
               margin="normal"
               value={session_description}
               rows={4}
-              onChange={(e) => setDescription(e.target.value)}
+              onChange={(e) =>
+                setSession((prevState) => ({
+                  ...prevState,
+                  session_description: e,
+                }))
+              }
             />
             <br />
 
@@ -111,11 +146,22 @@ export default function CreateSessionTextFields() {
               label="City"
               margin="normal"
               value={session_city}
-              onChange={(e) => setCity(e.target.value)}
+              onChange={(e) =>
+                setSession((prevState) => ({
+                  ...prevState,
+                  session_city: e,
+                }))
+              }
             />
           </Box>
 
-          <Box sx={{ alignItems: 'center', justifyContent: 'center', width: { sm: 450, md: 300, lg: 400 } }}>
+          <Box
+            sx={{
+              alignItems: "center",
+              justifyContent: "center",
+              width: { sm: 450, md: 300, lg: 400 },
+            }}
+          >
             <TextField
               required
               fullWidth
@@ -123,7 +169,12 @@ export default function CreateSessionTextFields() {
               label="Field Location"
               margin="normal"
               value={session_location}
-              onChange={(e) => setLocation(e.target.value)}
+              onChange={(e) =>
+                setSession((prevState) => ({
+                  ...prevState,
+                  session_location: e,
+                }))
+              }
             />
             <br />
 
@@ -136,7 +187,12 @@ export default function CreateSessionTextFields() {
               label="Equipment Needed"
               margin="normal"
               value={session_equipment_needed}
-              onChange={(e) => setEquipmentNeeded(e.target.value)}
+              onChange={(e) =>
+                setSession((prevState) => ({
+                  ...prevState,
+                  session_equipment_needed: e,
+                }))
+              }
             />
             <Tooltip title={equipmentInfo} arrow>
               <Button>MORE INFO</Button>
@@ -154,14 +210,21 @@ export default function CreateSessionTextFields() {
               InputLabelProps={{
                 shrink: true,
               }}
-              onChange={(e) => setPlayersNeeded(e.target.value)}
+              onChange={(e) =>
+                setSession((prevState) => ({
+                  ...prevState,
+                  session_players_needed: e,
+                }))
+              }
             />
           </Box>
         </Box>
       </Box>
-      <Stack sx={{
+      <Stack
+        sx={{
           paddingTop: 3,
-        }}>
+        }}
+      >
         <Button variant="contained">Submit</Button>
       </Stack>
     </div>
