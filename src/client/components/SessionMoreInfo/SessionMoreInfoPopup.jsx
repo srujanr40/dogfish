fe5f20987ce1431bad1fd7d2e5487051108e438c
rkelly03 <rkelly9812@gmail.com>
2023-06-12 18:32:59 -0700

    Component is now styled

--- src/client/components/SessionMoreInfo/SessionMoreInfoPopup.jsx
+++ src/client/components/SessionMoreInfo/SessionMoreInfoPopup.jsx
@@ -1,119 +1,49 @@
-import "./SessionMoreInfo.css";
-import { useSelector } from 'react-redux';
+import React from "react";
+import { useSelector } from "react-redux";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
+import CloseIcon from "@mui/icons-material/Close";
+import "./SessionMoreInfo.css";
 
-// props should contain a session ID and a closeModal callback for when the more info popup's close button is pressed.
 export default function SessionMoreInfoPopup(props) {
-  const sessions = useSelector(state => state.fetchSession);
-  const session = sessions.find(element => element.groupId === props.sessionID)
+  const sessions = useSelector((state) => state.fetchSession);
+  const session = sessions.find((element) => element.groupId === props.sessionID);
 
   return (
     <div id="session-more-info-popup">
       <div id="session-more-info-content">
         <Box>
-          <Typography
-            variant="h3"
-            gutterBottom
-            sx={{
-              justifyContent: "center",
-              fontWeight: "medium",
-            }}
-          >
-            Session Info
+          <Typography variant="h3" gutterBottom className="popup-title">
+            {session.name}
           </Typography>
-          <Box sx={{ display: "flex", flexDirection: { xs: "column", sm: "column", md: "row", lg: "row", } }}>
-            <div id="session-more-info-popup-text">
-              <Box className="main-text" sx={{
-                display: "flex",
-                flexDirection: { xs: "column", sm: "column", md: "column", lg: "row", }
-              }}>
-                <Box sx={{ alignItems: 'center', justifyContent: 'center', width: { sm: 450, md: 300, lg: 400 }, paddingRight: 2 }}>
-                  <Typography
-                    variant="h4"
-                    gutterBottom
-                    sx={{
-                      justifyContent: "center",
-                      fontWeight: "medium",
-                    }}
-                  >
-                    {session.name}
-                  </Typography>
-                  <br />
-
-                  <Typography
-                    variant="h4"
-                    gutterBottom
-                    sx={{
-                      justifyContent: "center",
-                      fontWeight: "medium",
-                    }}
-                  >
-                    Sport: {session.sport}
-                  </Typography>
-                  <br />
-
-                  <Typography
-                    variant="h5"
-                    gutterBottom
-                    sx={{
-                      justifyContent: "center",
-                      fontWeight: "medium",
-                    }}
-                  >
-                    {session.description}
-                  </Typography>
-                </Box>
+          <Box className="popup-details">
+            <div className="main-text">
+              <Box>
+                <Typography variant="h5" gutterBottom className="session-sport">
+                <b>Sport:</b> {session.sport}
+                </Typography>
+                <Typography variant="h5" gutterBottom className="session-description">
+                  <b>Description:</b> {session.description}
+                </Typography>
               </Box>
-
-              <Box className="additional-details" sx={{
-                display: "flex",
-                flexDirection: { xs: "column", sm: "column", md: "column", lg: "row", }
-              }}>
-                <Box sx={{ alignItems: 'center', justifyContent: 'center', width: { sm: 450, md: 300, lg: 400 }, paddingRight: 2 }}>
-                  <Typography
-                    variant="h6"
-                    gutterBottom
-                    sx={{
-                      justifyContent: "center",
-                      fontWeight: "medium",
-                    }}
-                  >
-                    Where: {session.location}, {session.city}
-                  </Typography>
-                  <br />
-
-                  <Typography
-                    variant="h6"
-                    gutterBottom
-                    sx={{
-                      justifyContent: "center",
-                      fontWeight: "medium",
-                    }}
-                  >
-                    Missing Equipment: {session.equipment}
-                  </Typography>
-                  <br />
-
-                  <Typography
-                    variant="h6"
-                    gutterBottom
-                    sx={{
-                      justifyContent: "center",
-                      fontWeight: "medium",
-                    }}
-                  >
-                    Missing Players: {session.playersNeeded}
-                  </Typography>
-                  <br />
-                </Box>
+            </div>
+            <div className="additional-details">
+              <Box>
+                <Typography variant="h5" gutterBottom className="session-location">
+                <b>Where:</b> {session.location}, {session.city}
+                </Typography>
+                <Typography variant="h5" gutterBottom className="session-equipment">
+                <b>Missing Equipment:</b> {session.equipment}
+                </Typography>
+                <Typography variant="h5" gutterBottom className="session-players-needed">
+                <b>Missing Players:</b> {session.playersNeeded}
+                </Typography>
               </Box>
             </div>
           </Box>
         </Box>
-
         <span className="close" onClick={props.closeModal}>
-          &times;
+          <CloseIcon />
         </span>
       </div>
     </div>
