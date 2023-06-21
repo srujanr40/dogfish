a8816eb2bf3be76e1ff8f4e3966ceb31f64d263e
Krish Sridar <krishkhavin@gmail.com>
2023-06-21 12:57:51 -0700

    Routed to new basic MySessions page

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,7 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import rootReducer from './client/reducers';
 import JoinSession from "./client/components/JoinSession/JoinSession";
+import MySessions from './client/components/MySessions/MySessions';
 
 const store = createStore(rootReducer);
 
@@ -20,6 +21,7 @@ function App() {
             <Route path="/" element={<Dashboard />} />
             <Route path="/dashboard" element={<Dashboard />} />
             <Route path="/join" element={<JoinSession />} />
+            <Route path="/mysessions" element={<MySessions />} />
             <Route path="/profile" element={<Profile />} />
           </Routes>
         </main>
---
+++ client/src/client/components/MySessions/MySessions.jsx
@@ -0,0 +1,53 @@
+import * as React from 'react';
+import Navbar from '../Navbar/Navbar.jsx';
+import Featured from '../FeaturedCard/Featured.jsx';
+import SessionCard from '../SessionCard/SessionCard.jsx';
+import './MySessions.css';
+import "../styles.module.css"
+import Divider from '@mui/material/Divider';
+import Fab from '@mui/material/Fab';
+import AddIcon from '@mui/icons-material/Add';
+import { useState } from 'react';
+import CreateSessionPopup from '../CreateSession/CreateSessionPopup.jsx';
+import SessionMoreInfoPopup from '../SessionMoreInfo/SessionMoreInfoPopup.jsx';
+import { useSelector } from "react-redux";
+
+
+export default function MySessions() {
+    const [isSessionMoreInfoModalOpen, setIsSessionMoreInfoModalOpen] = useState(false);
+    const [selectedSessionId, setSelectedItemId] = useState(null);
+    var sessions = useSelector(state => state.sessionReducer).sessions;
+
+    const openSessionMoreInfoModal = (itemId) => {
+        setSelectedItemId(itemId);
+        setIsSessionMoreInfoModalOpen(true)
+    };
+
+    const closeSessionMoreInfoModal = () => {
+        setSelectedItemId(null);
+        setIsSessionMoreInfoModalOpen(false)
+    };
+
+    return (
+        <div className="container">
+            <Navbar />
+                <Divider />
+            <div className="sessionsContainer">
+                <h4>Upcoming </h4>
+                <Divider />
+                <ul className="sessionsList">
+                    {sessions.map((element, index) => (
+                        <SessionCard key={index} name={element.name} description={element.description} groupId={element.groupId} onMoreInfo={openSessionMoreInfoModal} />
+                    ))}
+                </ul>            
+
+                {isSessionMoreInfoModalOpen && (
+                <div>
+                    <SessionMoreInfoPopup sessionID={selectedSessionId} closeModal={closeSessionMoreInfoModal} />
+                </div>
+                )}
+                
+            </div>
+        </div>
+    )
+}
