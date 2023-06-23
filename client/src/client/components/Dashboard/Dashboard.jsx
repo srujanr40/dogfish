05af0594071a7a98cb507963f9119c5978a651b6
Krish Sridar <krishkhavin@gmail.com>
2023-06-23 16:32:10 -0700

    fixed scrollbar

--- client/src/client/components/Dashboard/Dashboard.jsx
+++ client/src/client/components/Dashboard/Dashboard.jsx
@@ -12,66 +12,80 @@ import CreateSessionPopup from '../CreateSession/CreateSessionPopup.jsx';
 import SessionMoreInfoPopup from '../SessionMoreInfo/SessionMoreInfoPopup.jsx';
 import { useSelector } from "react-redux";
 
-
 export default function Dashboard() {
-    const [isCreateSessionModalOpen, setIsCreateSessionModalOpen] = useState(false);
-    const [selectedSessionId, setSelectedItemId] = useState(null);
-    var sessions = useSelector(state => state.sessionReducer).sessions;
+  const [isCreateSessionModalOpen, setIsCreateSessionModalOpen] = useState(false);
+  const [selectedSessionId, setSelectedItemId] = useState(null);
+  var sessions = useSelector(state => state.sessionReducer).sessions;
 
-    const [isSessionMoreInfoModalOpen, setIsSessionMoreInfoModalOpen] = useState(false);
+  const [isSessionMoreInfoModalOpen, setIsSessionMoreInfoModalOpen] = useState(false);
 
-    const openCreateSessionModal = () => {
-        setIsCreateSessionModalOpen(true);
-    };
+  const openCreateSessionModal = () => {
+    setIsCreateSessionModalOpen(true);
+  };
 
-    const closeCreateSessionModal = () => {
-        setIsCreateSessionModalOpen(false);
-    };
+  const closeCreateSessionModal = () => {
+    setIsCreateSessionModalOpen(false);
+  };
 
-    const openSessionMoreInfoModal = (itemId) => {
-        setSelectedItemId(itemId);
-        setIsSessionMoreInfoModalOpen(true)
-    };
+  const openSessionMoreInfoModal = (itemId) => {
+    setSelectedItemId(itemId);
+    setIsSessionMoreInfoModalOpen(true)
+  };
 
-    const closeSessionMoreInfoModal = () => {
-        setSelectedItemId(null);
-        setIsSessionMoreInfoModalOpen(false)
-    };
+  const closeSessionMoreInfoModal = () => {
+    setSelectedItemId(null);
+    setIsSessionMoreInfoModalOpen(false)
+  };
 
-
-    return (
-        <div className="container">
-            <Navbar />
-            <div className="featuredContainer">
-                <div className="featuredAndCreate">
-
-                </div>
-                <Divider />
-                <Featured />
-            </div>
-            <div className="sessionsContainer">
-                <h2 style={{color: 'white'}}>Activities near you</h2>
-                <Divider sx={{backgroundColor: 'gray'}}/>
-                <ul className="sessionsList">
-                    {sessions.map((element, index) => (
-                        <SessionCard joined={element.joined} key={index} name={element.name} description={element.description} groupId={element.groupId} onMoreInfo={openSessionMoreInfoModal} image={element.image} />
-                    ))}
-                </ul>
-                <h2 style={{color: 'white'}}>Newly added</h2>
-                <Divider sx={{backgroundColor: 'gray'}}/>
-                <ul className="sessionsList">
-                    {sessions.map((element, index) => (
-                        <SessionCard key={index} name={element.name} description={element.description} groupId={element.groupId} onMoreInfo={openSessionMoreInfoModal} image={element.image} />
-                    ))}
-                </ul>
-
-                {isSessionMoreInfoModalOpen && (
-                <div>
-                    <SessionMoreInfoPopup sessionID={selectedSessionId} closeModal={closeSessionMoreInfoModal} />
-                </div>
-                )}
-                
-            </div>
+  return (
+    <div className="container">
+      <Navbar />
+      <div className="featuredContainer">
+        <div className="featuredAndCreate"></div>
+        <Divider />
+        <Featured />
+      </div>
+      <div className="sessionsContainer">
+        <h2 style={{ color: 'white' }}>Activities near you</h2>
+        <Divider sx={{ backgroundColor: 'gray' }} />
+        <div className="scrollableContainer">
+          <ul className="sessionsList">
+            {sessions.map((element, index) => (
+              <SessionCard
+                joined={element.joined}
+                key={index}
+                name={element.name}
+                description={element.description}
+                groupId={element.groupId}
+                onMoreInfo={openSessionMoreInfoModal}
+                image={element.image}
+              />
+            ))}
+          </ul>
         </div>
-    )
+        <h2 style={{ color: 'white' }}>Newly added</h2>
+        <Divider sx={{ backgroundColor: 'gray' }} />
+        <div className="scrollableContainer">
+          <ul className="sessionsList">
+            {sessions.map((element, index) => (
+              <SessionCard
+                key={index}
+                name={element.name}
+                description={element.description}
+                groupId={element.groupId}
+                onMoreInfo={openSessionMoreInfoModal}
+                image={element.image}
+              />
+            ))}
+          </ul>
+        </div>
+
+        {isSessionMoreInfoModalOpen && (
+          <div>
+            <SessionMoreInfoPopup sessionID={selectedSessionId} closeModal={closeSessionMoreInfoModal} />
+          </div>
+        )}
+      </div>
+    </div>
+  )
 }
