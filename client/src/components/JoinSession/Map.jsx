b675dec9fbb938e4adc5d5d026e9f85bd9a69d36
Taqdeer Grewal <taqdeergrewal@Taqdeers-MacBook-Air.local>
2023-07-14 18:50:08 -0700

    google maps api

--- client/src/components/JoinSession/Map.jsx
+++ client/src/components/JoinSession/Map.jsx
@@ -2,32 +2,51 @@ import './JoinSession.css';
 import Navbar from '../Navbar/Navbar.jsx';
 import React from "react";
 import Box from "@mui/material/Box";
-import Divider from "@mui/material/Divider";
-import {useDispatch, useSelector} from 'react-redux';
-import {Link, useLocation} from "react-router-dom";
-import "react-chat-elements/dist/main.css"
-import { MessageList, Input } from "react-chat-elements"
-import Button from "@mui/material/Button";
-import { addChatAsync } from "../../redux/chat/chatThunks";
+import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
 
-import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
+/* Reference: https://www.npmjs.com/package/@react-google-maps/api */
+
+const containerStyle = {
+  width: '400px',
+  height: '400px'
+};
+
+const center = {
+  lat: -3.745,
+  lng: -38.523
+};
 
 export default function Map() {
-    const { isMapLoaded } = useLoadScript(
-        {
-            googleMapsApiKey: "AIzaSyAe4G85edT2a9BPunRUIV-WnVvON6DAquQ",
-        }
-    );
-    console.log(!isMapLoaded)
-
-    return (
-        (!isMapLoaded) && <><Box sx={{
-            width: 400,
-            height: 600,
-            backgroundColor: 'primary.dark',
-        }} /><GoogleMap
+    const { isLoaded } = useJsApiLoader({
+        id: 'google-map-script',
+        googleMapsApiKey: "AIzaSyAe4G85edT2a9BPunRUIV-WnVvON6DAquQ"
+      })
+    
+      const [map, setMap] = React.useState(null)
+    
+      const onLoad = React.useCallback(function callback(map) {
+        // This is just an example of getting and using the map instance!!! don't just blindly copy!
+        const bounds = new window.google.maps.LatLngBounds(center);
+        map.fitBounds(bounds);
+    
+        setMap(map)
+      }, [])
+    
+      const onUnmount = React.useCallback(function callback(map) {
+        setMap(null)
+      }, [])
+    
+      return isLoaded ? (
+          <GoogleMap
+            mapContainerStyle={containerStyle}
+            center={center}
             zoom={10}
-            center={{ lat: 44, lng: 84 }}
-        ></GoogleMap></>
-    );
-}
+            onLoad={onLoad}
+            onUnmount={onUnmount}
+          >
+            { /* Child components, such as markers, info windows, etc. */ }
+            <></>
+          </GoogleMap>
+      ) : <></>
+    }
+    
