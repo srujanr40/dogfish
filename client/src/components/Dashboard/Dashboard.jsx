234d5053337d2e5f7ad89ab058f4e3e9a02cf2d7
edvin <edvinkeskin@gmail.com>
2023-08-07 12:39:12 -0700

    added create session button to dashboard

--- client/src/components/Dashboard/Dashboard.jsx
+++ client/src/components/Dashboard/Dashboard.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, {useState, useEffect} from 'react';
 import Navbar from '../Navbar/Navbar.jsx';
 import Featured from '../FeaturedCard/Featured.jsx';
 import Card from '../Card/Card.jsx';
@@ -7,102 +7,112 @@ import '../styles.module.css';
 import Fab from '@mui/material/Fab';
 import AddIcon from '@mui/icons-material/Add';
 import CreateSessionPopup from '../CreateSession/CreateSessionPopup.jsx';
-import { getSessionsAsync, getSessionsNearYouAsync } from '../../redux/session/sessionThunks.js';
-import { useDispatch, useSelector } from 'react-redux';
+import {getSessionsAsync, getSessionsNearYouAsync} from '../../redux/session/sessionThunks.js';
+import {useDispatch, useSelector} from 'react-redux';
 
 export default function Dashboard() {
-  const profile = useSelector((state) => state.profileReducer).profile;
-  const [isCreateSessionModalOpen, setIsCreateSessionModalOpen] = useState(false);
-  const dispatch = useDispatch();
+    const profile = useSelector((state) => state.profileReducer).profile;
+    const [isCreateSessionModalOpen, setIsCreateSessionModalOpen] = useState(false);
+    const dispatch = useDispatch();
 
-  const [allSessions, setAllSessions] = useState([]);
-  const [nearYouSessions, setNearYouSessions] = useState([]);
-  const [frisbeeSessions, setFrisbeeSessions] = useState([]);
-  const [soccerSessions, setSoccerSessions] = useState([]);
-  const [filteredSessions, setFilteredSessions] = useState([]);
+    const [allSessions, setAllSessions] = useState([]);
+    const [nearYouSessions, setNearYouSessions] = useState([]);
+    const [frisbeeSessions, setFrisbeeSessions] = useState([]);
+    const [soccerSessions, setSoccerSessions] = useState([]);
+    const [filteredSessions, setFilteredSessions] = useState([]);
 
-  useEffect(() => {
-    fetchSessions();
-  }, [dispatch, profile.location]);
+    useEffect(() => {
+        fetchSessions();
+    }, [dispatch, profile.location]);
 
-  const fetchSessions = async () => {
-    try {
-      const allSessionsRes = await dispatch(getSessionsAsync(''));
-      const allSessionsData = allSessionsRes.payload;
-      setAllSessions(allSessionsData);
+    const fetchSessions = async () => {
+        try {
+            const allSessionsRes = await dispatch(getSessionsAsync(''));
+            const allSessionsData = allSessionsRes.payload;
+            setAllSessions(allSessionsData);
 
-      dispatch(getSessionsNearYouAsync(profile.location))
-        .then((sessions) => {
-          const near_you = sessions.payload;
-          setNearYouSessions(near_you);
-        })
-        .catch((error) => {
-          console.error('Error fetching near you sessions:', error);
-        });
+            dispatch(getSessionsNearYouAsync(profile.location))
+                .then((sessions) => {
+                    const near_you = sessions.payload;
+                    setNearYouSessions(near_you);
+                })
+                .catch((error) => {
+                    console.error('Error fetching near you sessions:', error);
+                });
 
-      const frisbeeSessionsData = allSessionsData.filter((session) => session.sport === 'Frisbee');
-      setFrisbeeSessions(frisbeeSessionsData);
+            const frisbeeSessionsData = allSessionsData.filter((session) => session.sport === 'Frisbee');
+            setFrisbeeSessions(frisbeeSessionsData);
 
-      const soccerSessionsData = allSessionsData.filter((session) => session.sport === 'Soccer');
-      setSoccerSessions(soccerSessionsData);
-    } catch (error) {
-      console.error('Error fetching sessions:', error);
-    }
-  };
+            const soccerSessionsData = allSessionsData.filter((session) => session.sport === 'Soccer');
+            setSoccerSessions(soccerSessionsData);
+        } catch (error) {
+            console.error('Error fetching sessions:', error);
+        }
+    };
 
-  const openCreateSessionModal = () => {
-    setIsCreateSessionModalOpen(true);
-  };
+    const openCreateSessionModal = () => {
+        setIsCreateSessionModalOpen(true);
+    };
 
-  const closeCreateSessionModal = () => {
-    setIsCreateSessionModalOpen(false);
-  };
+    const closeCreateSessionModal = () => {
+        setIsCreateSessionModalOpen(false);
+    };
 
-  const handleSessionCreated = () => {
-    fetchSessions();
-  };
-  const [searchTerm, setSearchTerm] = useState('');
+    const handleSessionCreated = () => {
+        fetchSessions();
+    };
+    const [searchTerm, setSearchTerm] = useState('');
 
-  const handleSearchSessions = (searchTerm) => {
-    setSearchTerm(searchTerm);
-    const filtered = allSessions.filter((session) =>
-      session.name.toLowerCase().includes(searchTerm.toLowerCase())
+    const handleSearchSessions = (searchTerm) => {
+        setSearchTerm(searchTerm);
+        const filtered = allSessions.filter((session) =>
+            session.name.toLowerCase().includes(searchTerm.toLowerCase())
+        );
+        setFilteredSessions(filtered);
+    };
+
+    return (
+        <div className="container">
+            <Navbar onSearch={handleSearchSessions}/>
+            <div className="sessionsContainer">
+                {searchTerm ? (
+                    <div className='searchContainer'>
+                        <div className="createSessionButton">
+                            <Fab variant="extended" color="primary" aria-label="create" onClick={openCreateSessionModal}>
+                                <AddIcon/>
+                                Create Session
+                            </Fab>
+                        </div>
+                        <Card sessions={filteredSessions} name={'Filtered Sessions'}/>
+                    </div>
+                ) : (
+                    <>
+                        <div className="createSessionButton">
+                            {!isCreateSessionModalOpen && (
+                                <Fab variant="extended" color="primary" aria-label="create"
+                                     onClick={openCreateSessionModal}>
+                                    <AddIcon/>
+                                    Create Session
+                                </Fab>
+                            )}
+
+                            {isCreateSessionModalOpen && (
+                                <div>
+                                    <CreateSessionPopup closeModal={closeCreateSessionModal}
+                                                        onSessionCreated={handleSessionCreated}/>
+                                </div>
+                            )}
+                        </div>
+                        <div className="featuredContainer">
+                            <Featured/>
+                        </div>
+                        <Card sessions={nearYouSessions} name={'Activities near you'}/>
+                        <Card sessions={frisbeeSessions} name={'Frisbee'}/>
+                        <Card sessions={soccerSessions} name={'Soccer'}/>
+                        <Card sessions={allSessions} name={'All'}/>
+                    </>
+                )}
+            </div>
+        </div>
     );
-    setFilteredSessions(filtered);
-  };
-
-  return (
-    <div className="container">
-      <Navbar onSearch={handleSearchSessions} />
-      <div className="sessionsContainer">
-        {searchTerm ? (
-          <Card sessions={filteredSessions} name={'Filtered Sessions'} />
-        ) : (
-          <>
-            <div className="createSessionButton">
-              {!isCreateSessionModalOpen && (
-                <Fab variant="extended" color="primary" aria-label="create" onClick={openCreateSessionModal}>
-                  <AddIcon />
-                  Create Session
-                </Fab>
-              )}
-
-              {isCreateSessionModalOpen && (
-                <div>
-                  <CreateSessionPopup closeModal={closeCreateSessionModal} onSessionCreated={handleSessionCreated} />
-                </div>
-              )}
-            </div>
-             <div className="featuredContainer">
-              <Featured />
-            </div>
-            <Card sessions={nearYouSessions} name={'Activities near you'} />
-            <Card sessions={frisbeeSessions} name={'Frisbee'} />
-            <Card sessions={soccerSessions} name={'Soccer'} />
-            <Card sessions={allSessions} name={'All'} />
-          </>
-        )}
-      </div>
-    </div>
-  );
 }
