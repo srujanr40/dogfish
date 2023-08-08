76a6f4227baa1e0acb124513f881fea54b070bdf
Krish Sridar <krishkhavin@gmail.com>
2023-08-07 17:35:29 -0700

    Featured behaves like Card; calls sessionCards

--- client/src/components/FeaturedCard/Featured.jsx
+++ client/src/components/FeaturedCard/Featured.jsx
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { useState }from 'react';
 import { useSelector } from 'react-redux';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
-import { Card, CardContent, CardActions, CardMedia, Typography, Button } from '@mui/material';
+import { Card } from '@mui/material';
+import SessionCard from '../SessionCard/SessionCard';
+import SessionMoreInfoPopup from '../SessionMoreInfo/SessionMoreInfoPopup';
 
-const Featured = () => {
+const FeaturedCard = () => {
   const featuredSessions = useSelector((store) => store.sessionReducer).featuredSessions;
+
+  const [isSessionMoreInfoModalOpen, setIsSessionMoreInfoModalOpen] =
+  useState(false);
+const [selectedSessionId, setSelectedItemId] = useState(null);
+
+const openSessionMoreInfoModal = (itemId) => {
+  setSelectedItemId(itemId);
+  setIsSessionMoreInfoModalOpen(true);
+};
+
+const closeSessionMoreInfoModal = () => {
+  setSelectedItemId(null);
+  setIsSessionMoreInfoModalOpen(false);
+};
 
   const settings = {
     dots: true,
@@ -20,41 +36,34 @@ const Featured = () => {
   };
 
   return (
+    <>
     <Card
       sx={{
         backgroundColor: '#1C1E25',
         maxWidth: '100%',
         margin: '0 auto',
-        borderRadius: '10px'
+        borderRadius: '10px',
+        transition: 'transform 0.2s',
       }}
+      onMouseEnter={(e) => (e.currentTarget.style.transform = 'scale(1.005')}
+      onMouseLeave={(e) => (e.currentTarget.style.transform = 'none')}
     >
       <Slider {...settings}>
         {featuredSessions.map((session, index) => (
-          <div key={index}>
-            <CardMedia component="img" height="300" image={session.image} alt={`Slide ${index + 1}`} />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="div" sx={{ color: 'white' }}>
-                {session.name}
-              </Typography>
-              <Typography variant="body2" color="text.secondary" sx={{ color: 'white' }}>
-                {session.description}
-              </Typography>
-            </CardContent>
-            <CardActions>
-              <Button sx={{ color: 'white', backgroundColor: '#DD4D2B', textTransform: 'none', '&:hover': { backgroundColor: '#FF6E6E' } }} size="small">
-                Join
-              </Button>
-              <Button
-                sx={{ color: 'black', backgroundColor: '#4D96B6', textTransform: 'none', '&:hover': { backgroundColor: '#FFD43D' } }}
-                size="small"
-              >                More Info
-              </Button>
-            </CardActions>
-          </div>
+          <SessionCard key={index} session={session} featured={true} onMoreInfo={openSessionMoreInfoModal}/>
         ))}
       </Slider>
     </Card>
+    {isSessionMoreInfoModalOpen && (
+        <div>
+          <SessionMoreInfoPopup
+            sessionID={selectedSessionId}
+            closeModal={closeSessionMoreInfoModal}
+          />
+        </div>
+      )}
+    </>
   );
 };
 
-export default Featured;
+export default FeaturedCard;
--- client/src/components/SessionCard/SessionCard.jsx
+++ client/src/components/SessionCard/SessionCard.jsx
@@ -38,6 +38,7 @@ export default function SessionCard(props) {
 
   const isMember = props.session.members.some(member => member && member.email === profile.email)
 
+  const isFeatured = props.featured;
   const formattedDate = new Date(props.session.dateTime).toLocaleDateString('en-US', {
     // weekday: 'short',
     month: 'short',
@@ -50,9 +51,8 @@ export default function SessionCard(props) {
         <Card
           className={`session-card ${isMember ? 'member' : ''}`}
           sx={{
-            maxWidth: '250px',
             minWidth: '250px',
-            minHeight: '315px',
+            minHeight: `${isFeatured ? "415px" : "315px"}`,
             maxHeight: '315px',
             backgroundColor: '#1C1E25',
             display: 'flex',
@@ -60,12 +60,12 @@ export default function SessionCard(props) {
             borderRadius: '10px',
             transition: 'transform 0.2s',
           }}
-          onMouseEnter={(e) => (e.currentTarget.style.transform = 'scale(1.02)')}
+          onMouseEnter={(e) => (e.currentTarget.style.transform = `${isFeatured? '' :'scale(1.02)' }` )}
           onMouseLeave={(e) => (e.currentTarget.style.transform = 'none')}
         >
           <CardMedia
             component="img"
-            height="150"
+            height={`${isFeatured ? "250" : "150"}`}
             image={props.session.image}
             alt="image"
           />
@@ -115,9 +115,8 @@ export default function SessionCard(props) {
       <Card
         className={`session-card ${isMember ? 'member' : ''}`}
         sx={{
-          maxWidth: '250px',
           minWidth: '250px',
-          minHeight: '315px',
+          minHeight: `${isFeatured ? "415px" : "315px"}`,
           maxHeight: '315px',
           backgroundColor: '#1C1E25',
           display: 'flex',
@@ -126,13 +125,13 @@ export default function SessionCard(props) {
           transition: 'transform 0.2s',
         }}
 
-        onMouseEnter={(e) => (e.currentTarget.style.transform = 'scale(1.02)')}
+        onMouseEnter={(e) => (e.currentTarget.style.transform = `${isFeatured? '' :'scale(1.02)' }` )}
         onMouseLeave={(e) => (e.currentTarget.style.transform = 'none')}
         onClick={handleOpenMoreInfo}
       >
         <CardMedia
           component="img"
-          height="150"
+          height={`${isFeatured ? "250" : "150"}`}
           image={props.session.image}
           alt="image"
         />
