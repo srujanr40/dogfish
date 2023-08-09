2f9cc4d93ede6d36e5e0a74ee3b7287fa2bd9f1b
Krish Sridar <krishkhavin@gmail.com>
2023-08-09 00:12:15 -0700

    ui updates

--- client/src/components/JoinSession/Map.jsx
+++ client/src/components/JoinSession/Map.jsx
@@ -2,8 +2,7 @@ import React, { useEffect, useState } from "react";
 import { GoogleMap, Marker, useJsApiLoader } from "@react-google-maps/api";
 
 const containerStyle = {
-  width: '400px',
-  height: '600px'
+  height: '700px'
 };
 
 export default function Map(props) {
